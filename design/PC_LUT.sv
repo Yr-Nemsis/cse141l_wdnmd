299573de2f4fcb0729f6c30e4542095354307332
elllisonLi <89509181+elllisonLi@users.noreply.github.com>
2023-03-12 13:26:32 -0700

    Update PC_LUT.sv

--- design/PC_LUT.sv
+++ design/PC_LUT.sv
@@ -8,31 +8,33 @@ module PC_LUT #(parameter D=12)(
   always_comb case(addr)
     // program 3
     0: target = 11;
-	1: target = 15;
-	2: target = -17;
+    1: target = 15;
+    2: target = -17;
     3: target = 4;
     4: target = -28;
-	5: target = -39;
-	6: target = 6;
+    5: target = -39;
+    6: target = 6;
     7: target = -30;
     8: target = -18;
-	9: target = -26;
-	10: target = -29;
+    9: target = -26;
+    10: target = -29;
     11: target = -39;
     12: target = -41;
-	13: target = -62;
-	14: target = 12'b0;
+    13: target = -62;
+    14: target = 12'b0;
     15: target = 12'b0;
     //program 1
     16: target = 14;  //beq end
     17: target = -143; //beq loop
     //program 2
-    18: target = 2;   //beq skip1 on line 87
-    19: target = 21;  //beq skip2 on line 100
-    20: target = 7;   //beq small on line 112
-    21: target = 3;   //beq skip2 on line 118
-    22: target = 14;  //beq end on line 144
-    23: target = -137; //beq loop on line 157
+    18: target = -174;   //beq loop on line 194
+    19: target = 14;  //beq end on line 181
+    20: target = 9;   //beq small on line 104
+    21: target = 14;   //beq hello on line 112
+    22: target = 7;  //beq hello on line 119
+    23: target = 3; //beq hello on line 123
+    26: target = 37; //beq skip1 on line 87
+    27: target = 28; //beq skip2 on line 92	 
     //program 3
     24: target = -37;
     25: target = -48;
